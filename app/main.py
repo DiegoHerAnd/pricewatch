b0cf9bf5f92ed4d9636637d938407750d76871a1
DiegoHerAnd <diegoherand06@gmail.com>
2026-05-09 16:16:16 +0200

    style: cambio de swagger a scalar

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from app.routers import products, categories, stores
+from scalar_fastapi import get_scalar_api_reference
+app = FastAPI(
+    title="PriceWatch API",
+    version="1.0.0",
+    docs_url=None,
+    redoc_url=None
+)
 
-app = FastAPI(title="PriceWatch API", version="1.0.0")
-
+@app.get("/docs", include_in_schema=False)
+def scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url="/openapi.json",
+        title="PriceWatch API"
+    )
 app.include_router(products.router,   prefix="/api/v1")
 app.include_router(categories.router, prefix="/api/v1")
 app.include_router(stores.router,     prefix="/api/v1")
